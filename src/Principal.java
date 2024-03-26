b3acfa55a75d66d3aa3792425e050ec34b6f98a3
Nicollas Monteiro Barros <tr26351es@trf1.gov.br>
2024-03-26 17:41:13 -0300

    Envio do Dia 26.03

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -42,6 +44,16 @@ public class Principal {
         calculadora.inclui(outroFilme);
         calculadora.inclui(twd);
         System.out.println("A duração total é de: " +calculadora.getTempoTotal());
+
+	   FiltroRecomendacao filtro = new FiltroRecomendacao();
+	   filtro.filtra(meuFilme);
+
+	   Episodio episodio = new Episodio();
+	   episodio.setNumero(1);
+	   episodio.setSerie(twd);
+	   episodio.setTotalVizualizacoes(300);
+
+	   filtro.filtra(episodio);
 
     }
 }
--- src/br/com/alura/screenmatch/modelos/Episodio.java
+++ src/br/com/alura/screenmatch/modelos/Episodio.java
@@ -1,11 +1,22 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Episodio {
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Episodio implements Classificavel {
     private int numero;
     private String nome;
     private Serie serie;
+	private int totalVizualizacoes;
 
-    public int getNumero() {
+	public int getTotalVizualizacoes() {
+		return totalVizualizacoes;
+	}
+
+	public void setTotalVizualizacoes(int totalVizualizacoes) {
+		this.totalVizualizacoes = totalVizualizacoes;
+	}
+
+	public int getNumero() {
         return numero;
     }
 
@@ -28,4 +39,13 @@ public class Episodio {
     public void setSerie(Serie serie) {
         this.serie = serie;
     }
+
+	@Override
+	public int getClassificacao() {
+		if (totalVizualizacoes > 100){
+			return 4;
+		} else {
+			return 2;
+		}
+	}
 }
